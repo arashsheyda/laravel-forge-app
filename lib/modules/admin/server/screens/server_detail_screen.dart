053a08d44d11d069798322bd13277abc36f61460
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-10 09:20:10 +0300

    fix: add database card

--- lib/modules/admin/server/screens/server_detail_screen.dart
+++ lib/modules/admin/server/screens/server_detail_screen.dart
@@ -10,6 +10,39 @@ class ServerDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Server $serverId'),
       ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/server/detail/database');
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFF8EDAFE),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: Image.network(
+                      'https://cdn-icons-png.flaticon.com/512/2232/2232189.png',
+                      color: const Color(0xFF012458),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
