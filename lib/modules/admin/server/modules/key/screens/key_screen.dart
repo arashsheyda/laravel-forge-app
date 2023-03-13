179bcdcba9b242c8ce8202c2afd54a3ccfc00454
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-13 13:26:16 +0300

    fix: key screen ui

--- lib/modules/admin/server/modules/key/screens/key_screen.dart
+++ lib/modules/admin/server/modules/key/screens/key_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:forge/core/shared/appbar.dart';
+import 'package:forge/core/shared/widgets/table.dart';
 import 'package:forge/core/styles/colors.dart';
+import 'package:forge/modules/admin/server/modules/key/cubit/key_cubit.dart';
 
 class KeyScreen extends StatelessWidget {
   final int serverId;
@@ -9,11 +12,46 @@ class KeyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(
+            context,
+            '/server/detail/key/create',
+            arguments: serverId,
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBarWidget(
         title: 'SSH Keys',
         color: [colorSecondary, colorPrimary],
       ),
-      body: Container(),
+      body: BlocBuilder<KeyCubit, KeyState>(
+        bloc: context.read<KeyCubit>()..fetchAll(serverId: serverId),
+        builder: (context, state) {
+          if (state.pending) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return TableWidget(
+            columns: const [
+              'name',
+              'username',
+              'status',
+              'created_at',
+            ],
+            rows: state.keys
+                .map((item) => {
+                      'name': item.name,
+                      'status': item.status,
+                      'username': item.username,
+                      'created_at': item.created_at,
+                    })
+                .toList(),
+          );
+        },
+      ),
     );
   }
 }
