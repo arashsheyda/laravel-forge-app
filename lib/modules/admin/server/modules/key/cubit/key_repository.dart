5c9e942c7693e1b476f718b8071531368d38363b
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-13 13:22:20 +0300

    fix: fixed endpoint

--- lib/modules/admin/server/modules/key/cubit/key_repository.dart
+++ lib/modules/admin/server/modules/key/cubit/key_repository.dart
@@ -5,8 +5,7 @@ import 'package:forge/modules/admin/server/modules/key/models/key.dart';
 class KeyRepository {
   Future fetchAll({required int serverId}) async {
     try {
-      final response =
-          await ApiService().dio.get('/servers/$serverId/databases');
+      final response = await ApiService().dio.get('/servers/$serverId/keys');
       final keys = response.data['keys'] as List;
       return keys.map((e) => Key.fromJson(e)).toList();
       // ignore: unused_catch_clause
