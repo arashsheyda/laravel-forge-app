3e96df83ba4882b1a7993b144c779c0f640ea8ad
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-14 16:19:17 +0300

    fix: onboarding design base

--- lib/modules/onboarding/screens/onboarding_screen.dart
+++ lib/modules/onboarding/screens/onboarding_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:forge/core/styles/colors.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:forge/core/cubit/setting/setting_cubit.dart';
-import 'package:forge/modules/onboarding/widgets/onboarding_content.dart';
+import 'package:forge/modules/onboarding/widgets/page_section.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -23,6 +23,20 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     _pageController = PageController();
   }
 
+  final List<Widget> slides = [
+    const PageSectionWidget(
+      title: 'Server management doesn\'t have to be a nightmare',
+      description: 'Provision and deploy unlimited PHP applications & more',
+      image: 'assets/images/onboarding/02.png',
+    ),
+    const PageSectionWidget(
+      title: 'Deploy your application in minutes',
+      description:
+          'Forge is already making life easier for 534,653 applications',
+      image: 'assets/images/onboarding/01.png',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,46 +46,31 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           backgroundColor: Colors.transparent,
           elevation: 0,
           systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarIconBrightness: Brightness.dark,
+            statusBarIconBrightness: Brightness.light,
           ),
         ),
       ),
-      backgroundColor: Theme.of(context).brightness == Brightness.dark
-          ? colorOnBoardingDark
-          : colorOnBoardingLight,
-      body: PageView.builder(
-        physics: const BouncingScrollPhysics(),
-        controller: _pageController,
-        onPageChanged: (index) => setState(() => currentIndex = index),
-        itemCount: slides.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(20),
-            child: slides[index],
-          );
-        },
-      ),
-      bottomNavigationBar: Container(
-        color: Theme.of(context).colorScheme.background,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            AnimatedOpacity(
-              duration: const Duration(milliseconds: 200),
-              opacity: currentIndex == slides.length - 1 ? 0 : 1,
-              child: MaterialButton(
-                onPressed: () => _pageController.animateToPage(
-                  slides.length - 1,
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.ease,
-                ),
-                child: Text(
-                  AppLocalizations.of(context)!.skip,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Flexible(
+            child: PageView.builder(
+              physics: const BouncingScrollPhysics(),
+              controller: _pageController,
+              onPageChanged: (index) => setState(() => currentIndex = index),
+              itemCount: slides.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: slides[index],
+                );
+              },
             ),
-            Row(
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.53,
+            child: Row(
               children: [
                 for (var slide in slides) ...[
                   AnimatedContainer(
@@ -82,37 +81,62 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     height: 7,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(50),
-                      color: Theme.of(context).colorScheme.onBackground,
+                      color: slides.indexOf(slide) == currentIndex
+                          ? colorPrimary
+                          : Theme.of(context).colorScheme.onBackground,
                     ),
                   ),
                 ],
               ],
             ),
-            AnimatedSwitcher(
-              duration: const Duration(milliseconds: 200),
-              child: currentIndex == slides.length - 1
-                  ? MaterialButton(
-                      onPressed: () =>
-                          context.read<SettingCubit>().setOnboarding(),
-                      color: Colors.green,
-                      child: Text(
-                        AppLocalizations.of(context)!.start,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    )
-                  : MaterialButton(
-                      onPressed: () => _pageController.nextPage(
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.ease,
-                      ),
-                      child: Text(
-                        AppLocalizations.of(context)!.next,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                    ),
+          ),
+          Positioned(
+            bottom: 10,
+            child: MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 50,
+                vertical: 15,
+              ),
+              onPressed: () => _pageController.animateToPage(
+                slides.length - 1,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.ease,
+              ),
+              child: Text(
+                AppLocalizations.of(context)!.skip,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
             ),
-          ],
-        ),
+          ),
+          Positioned(
+            bottom: 10,
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 200),
+              opacity: currentIndex == slides.length - 1 ? 1 : 0,
+              child: IgnorePointer(
+                ignoring: currentIndex != slides.length - 1,
+                child: MaterialButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  color: colorPrimary,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 15,
+                  ),
+                  onPressed: () => context.read<SettingCubit>().setOnboarding(),
+                  child: Text(
+                    AppLocalizations.of(context)!.start,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/modules/onboarding/widgets/page_section.dart
+++ lib/modules/onboarding/widgets/page_section.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:forge/core/utils/storage.dart';
 
 class PageSectionWidget extends StatelessWidget {
+  final String? image;
   final String? animation;
   final String title;
   final String description;
   const PageSectionWidget({
     super.key,
+    this.image,
     this.animation,
     required this.title,
     required this.description,
@@ -15,19 +18,33 @@ class PageSectionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        image != null
+            ? Image.asset(
+                image!,
+              )
+            : const SizedBox.shrink(),
         animation != null
             ? Storage.animation('$animation.json', width: 250, height: 250)
-            : const SizedBox(),
+            : const SizedBox.shrink(),
+        const SizedBox(height: 100),
         Text(
           title,
-          style: Theme.of(context).textTheme.headlineSmall,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 50),
         Text(
           description,
-          style: Theme.of(context).textTheme.bodyLarge,
+          textAlign: TextAlign.center,
+          style: Theme.of(context)
+              .textTheme
+              .headlineMedium!
+              .copyWith(fontSize: 13),
         ),
       ],
     );
