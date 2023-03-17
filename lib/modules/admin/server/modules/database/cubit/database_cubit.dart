d90d01fddfd3b3319e75e892e90d7d56a8e5d4cd
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-17 16:47:25 +0300

    fix: fixed reloading data

--- lib/modules/admin/server/modules/database/cubit/database_cubit.dart
+++ lib/modules/admin/server/modules/database/cubit/database_cubit.dart
@@ -5,12 +5,16 @@ import 'package:forge/modules/admin/server/modules/database/cubit/database_repos
 part 'database_state.dart';
 part 'database_cubit.freezed.dart';
 
+var currentServer;
+
 class DatabaseCubit extends Cubit<DatabaseState> {
   DatabaseRepository repository;
   DatabaseCubit(this.repository) : super(DatabaseState.initial());
 
   Future<void> fetchAll({required int serverId}) async {
+    if (state.pending || currentServer == serverId) return;
     try {
+      currentServer = serverId;
       emit(state.copyWith(pending: true));
       final databases = await repository.fetchAll(serverId: serverId);
       emit(state.copyWith(databases: databases, pending: false));
