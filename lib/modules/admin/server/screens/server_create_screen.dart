5bb255dca1219fe130688bfece29ab336cbb1606
arashsheyda <sheidaeearash1999@gmail.com>
2023-03-20 09:21:40 +0300

    feat: server create screen

---
+++ lib/modules/admin/server/screens/server_create_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ServerCreateScreen extends StatelessWidget {
+  const ServerCreateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Server'),
+      ),
+    );
+  }
+}
